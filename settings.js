1f94a9bb7bdab55728294e2a8f9babdbdf4d77da
pavelwiz <139655962+pavelwiz@users.noreply.github.com>
2023-07-18 22:28:15 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -16,8 +16,8 @@ global.ONLY_GROUPS = false // Only Groups Use
 /*
 🤖 𝗕𝗢𝗧 𝗜𝗡𝗙𝗢 𝗦𝗘𝗧𝗧𝗜𝗡𝗚𝗦
 */
-global.OWNER_NAME = 'Dark Maker Ofc' // Owner Name
-global.OWNER_NUMBER = ['94715166712'] //Owner Number
+global.OWNER_NAME = 'Dark Maker Ofc' // Pavel
+global.OWNER_NUMBER = ['94715166712'] //0706337089
 global.BOT_ADMINS = ['94715166712','94715166712'] //Bot admins numbers Like a Sudo Numbers
 global.BLOCK_CHAT = [] // If you want Block Bot From Chats Use .jid Command and put Jids
 global.IMAGE_QUALITY = 'low' // Available Quality Are => low | mediam | high
