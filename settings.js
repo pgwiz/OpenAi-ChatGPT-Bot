bbbf4c0e91a22885593bec5714e13b9a9d538bd1
Natious <141484837+destofv9@users.noreply.github.com>
2023-09-13 04:09:29 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -18,7 +18,7 @@ global.ONLY_GROUPS = false // Only Groups Use
 */
 global.OWNER_NAME = 'mullah' // Owner Name
 global.OWNER_NUMBER = ['254794110821'] //Owner Number
-global.BOT_ADMINS = ['25490627796','254794110821'] //Bot admins numbers Like a Sudo Numbers
+global.BOT_ADMINS = ['254790627796','254794110821'] //Bot admins numbers Like a Sudo Numbers
 global.BLOCK_CHAT = [ '120363147404508506@g.us', '120363030304247368@g.us'] // If you want Block Bot From Chats Use .jid Command and put Jids
 global.IMAGE_QUALITY = 'high' //Available Quality Are => low | mediam | high
 global.CAPTION = '© ALWAYS MULLAH'
@@ -36,15 +36,15 @@ global.SESSION_ID = 'elisa_md@;;;EdVDkJYB#8gQrpbi99TQ83AzymWXOX3HC2JuNQGO8vQrX2v
 global.CMD1 = 'ai' // .ai Command
 global.CMD2 = 'gpt' // .ai2 Command
 global.IMAGE_CMD = 'aimg' // .aiimg Command 
-global.PORT = process.env.PORT || '4050' // Port No need Change.
+global.PORT = process.env.PORT || '4053' // Port No need Change.
 
 mess = {
-	owner: '*This Command is restricted 😮.*',
-	need_text: '*what do you need mehn*'
+	owner: '*This Command is restricted by Mullah  😮.*',
+	need_text: '*what do can i do *'
 }
 react = {
     ai_message : '🤖',
-    ai_image : '🤖'
+    ai_image : '📸'
 }
 
 
